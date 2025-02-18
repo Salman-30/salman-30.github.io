54e124189c2a17a563c4022b69954b1223b89a0f
Fuad-073 <alvinahmadfardhani3@gmail.com>
2025-02-18 18:26:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const display = document.getElementById("display")
+const display = document.getElementById("display");
 
 function appendToDisplay(input){
     display.value += input;
@@ -9,11 +9,10 @@ function clearDisplay(){
 }
 
 function calculate(){
-try{
-     display.value = eval;(display.value);
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
 }
-catch(error){
-     display.value = "Error";
-}
-}
-
