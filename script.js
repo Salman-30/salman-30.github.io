24e11d480b99647644eb8b03249d7bf9b2596f80
Fuad-073 <alvinahmadfardhani3@gmail.com>
2025-02-18 16:45:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,11 +10,10 @@ function clearDisplay(){
 
 function calculate(){
 try{
-
-    display.value = eval;(display.value);
+     display.value = eval;(display.value);
 }
 catch(error){
-    display.value = "Error";
+     display.value = "Error";
 }
 }
 
