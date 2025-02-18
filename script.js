4173f7304a8c83b0f7c2afcffffa9a17b3e7d525
Fuad-073 <alvinahmadfardhani3@gmail.com>
2025-02-18 16:44:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
 const display = document.getElementById("display")
 
-document.getElementById("display");
-
 function appendToDisplay(input){
-
-display.value += input;
-
+    display.value += input;
 }
 
-function clearDisplay(){ display.value = "";
-
+function clearDisplay(){
+    display.value = "";
 }
 
-function backspace(){ display.value -= }
 function calculate(){
-
 try{
 
-display.value = eval(display.value);
-
+    display.value = eval;(display.value);
 }
-
-catch(error){ display.value = "Error"; }
+catch(error){
+    display.value = "Error";
+}
 }
 
