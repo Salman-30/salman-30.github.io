ef5bc1c22dd06f06602dbdb4ce60ebeabd6b227d
Fire <dumpsterfireprogramming@gmail.com>
2025-02-28 11:00:11 +0700

    NewCompleteRNG

--- RNG/rngscript.js
+++ RNG/rngscript.js
@@ -1,1 +1,11 @@
-const randomNumber = Math.floor(Math.random() * 100)
+const randomNumber = Math.floor(Math.random() * 10)
+console.log(randomNumber)
+function checkNumber() {
+    let userInput = document.getElementById("numberInput").value
+    if (userInput == randomNumber) {
+        document.getElementById("result").innerHTML = "You Win!"
+    }
+    else {
+        document.getElementById("result").innerHTML = "You Lose!"
+    }
+}
